3008d4163e861a361f6256adee685334cf481cb3
chengbo <827185371@qq.com>
2022-01-15 18:09:11 +0800

    修改个人中心页面

--- dysx/dysx/MineViewController.m
+++ dysx/dysx/MineViewController.m
@@ -10,7 +10,7 @@
 
 #define lStatusBarHeiht [CustomTools getStatusBarHeight]
 
-@interface MineViewController ()
+@interface MineViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (nonatomic, strong) UIScrollView *scrollView;
 
@@ -33,7 +33,7 @@
     topView.backgroundColor = MAIN_COLOR;
     [self.view addSubview:topView];
     UIView *usrInfoSuperView = [[UIView alloc] initWithFrame:LRect(0, lStatusBarHeiht, topView.width, topView.height - lStatusBarHeiht)];
-    usrInfoSuperView.backgroundColor = [UIColor yellowColor];
+    usrInfoSuperView.backgroundColor = [UIColor greenColor];
     [topView addSubview:usrInfoSuperView];
     //创建用户头像视图
     UIImageView *usrImgView = [[UIImageView alloc] init];
@@ -47,7 +47,7 @@
     UILabel *usrLabel = [[UILabel alloc] init];
     usrLabel.backgroundColor = [UIColor blueColor];
     usrLabel.text= @"13971500541";
-    usrLabel.font = [UIFont systemFontOfSize:20.f];
+    usrLabel.font = [UIFont systemFontOfSize:24.f];
     [usrInfoSuperView addSubview:usrLabel];
     [usrLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(usrImgView.mas_right).offset(15);
@@ -56,25 +56,41 @@
         make.height.mas_equalTo(50);
     }];
     
-    /*
-    _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    _scrollView.y = lStatusBarHeiht;
-    NSLog(@"lStatusBarHeiht = %f", lStatusBarHeiht);
-    _scrollView.contentSize = CGSizeMake(SCREEN_WIDTH, SCREEN_HEIGHT + 100.f);
-    _scrollView.backgroundColor = [UIColor yellowColor];
-    _scrollView.showsVerticalScrollIndicator = YES;
-    [self.view addSubview:_scrollView];
-    UIView *topView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 132.f)];
-    topView.backgroundColor = MAIN_COLOR;
-    [_scrollView addSubview:topView];
-    UIImageView *usrImageView = [[UIImageView alloc] initWithFrame:CGRectMake(20.f, 100.f, 50.f, 50.f)];
-    UIView *usrNickName = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100.f, 50.f)];
-//    usrNickName.backgroundColor = MAIN_COLOR;
-    [_scrollView addSubview:usrNickName];
-     
-     */
-    
+    UITableView *mineTableView = [[UITableView alloc] initWithFrame:LRect(0, 200, SCREEN_WIDTH, 500)];
+    mineTableView.delegate = self;
+    mineTableView.dataSource = self;
+    mineTableView.backgroundColor = [UIColor purpleColor];
+//    [mineTableView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(topView.mas_bottom);
+//        make.bottom.equalTo(self.tabBarController.tabBar.mas_top);
+//        make.width.mas_equalTo(SCREEN_WIDTH);
+//        make.left.equalTo(topView.mas_left);
+//    }];
+    [self.view addSubview:mineTableView];
 }
+
+#pragma mark - UITableViewDataSource Methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *reuseCellId = @"UITableViewCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseCellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:reuseCellId];
+    }
+    return nil;
+}
+
+#pragma mark - UITableViewDelegate Methods
+
+
 
 //- (UIStatusBarStyle)preferredStatusBarStyle {
 //    return UIStatusBarStyleLightContent;
